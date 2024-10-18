16075920a8ac40ec38adf312d514cf6073d851f4
TryAgain1122 <luisrafhael1122@gmail.com>
2024-10-19 00:30:39 +0800

    navBar Props  updated

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,21 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Gallery from "./pages/Gallery"
+import { useEffect, useState } from "react"
 function App() {
+  const [textColor, setTextColor] = useState('text-black');
 
-  return (
+  useEffect(() => {
+    const backgroundColor = "white";
+    if (backgroundColor === "white") {
+      setTextColor("text-black")
+    } else {
+      setTextColor("text-white")
+    }
+  },[])
+  return ( 
    <BrowserRouter>
-   <Navbar />
+   <Navbar textColor={textColor}/>
     <Routes> 
      <Route path="/" element={<Home />}></Route>
      <Route path="/about" element={<About />}></Route>
